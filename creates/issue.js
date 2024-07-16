44c704a53da201b74f2af6fefb6b10babf98d2e0
kathryn-moon-xylophone <kathryn@caseproof.com>
2024-07-15 22:28:20 -0400

    the "Create" action in Zapier;

--- creates/issue.js
+++ creates/issue.js
@@ -1,8 +1,11 @@
 const sample = require('../samples/sample_issue');
-
+// A Create Action shows up in the Action step
 const createIssue = (z, bundle) => {
   const responsePromise = z.request({
     method: 'POST',
+    // Defining the interaction with your API endpoint. The bundle
+    // parameter holds data input and authentication information
+    // from the Zap:
     url: `https://api.github.com/repos/${bundle.inputData.repo}/issues`,
     body: JSON.stringify({
       title: bundle.inputData.title
@@ -12,18 +15,19 @@ const createIssue = (z, bundle) => {
     .then(response => JSON.parse(response.content));
 };
 
-module.exports = {
+module.exports = { // export an object 
   key: 'issue',
   noun: 'Issue',
 
   display: {
+    // What the user will see in the Zap Editor when selecting an action
     label: 'Create Issue',
     description: 'Creates an issue.'
   },
 
   operation: {
     inputFields: [
-      {key: 'repo', label:'Repo', required: true, dynamic: 'repo.full_name.full_name'},
+      {key: 'repo', label:'Repo', required: true, dynamic: 'repo.full_name.full_name'}, // Creates a dynamic drop-down that's populated by the 'repo' trigger
       {key: 'title', label:'Title', required: true},
       {key: 'body', label:'Body', required: false}
     ],
